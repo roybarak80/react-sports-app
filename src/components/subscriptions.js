b98f6e35b825b26cbaa32005b3680b4b5ed69410
Roy Barak <roy.barak@tcwd.fr>
2018-01-17 17:59:35 +0200

    Sec commit

---
+++ src/components/subscriptions.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+
+class SubScriptions extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            email: '',
+            error: false,
+            success: false
+        }
+    }
+
+    saveSubscriptions = (email) => {
+        //subcriptions
+        const URL_EMAIL = 'http://localhost:3004/subcriptions';
+        fetch(URL_EMAIL,{
+            method:"POST",
+            headers:{
+                'Accept':'application/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({email})
+
+        }).then(res=>res.json()).then(()=>{
+            this.setState({
+                email:'',
+                success:true
+            })
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let email = this.state.email;
+        let regex = /\S+@\S+\.\S+/;
+        if(regex.test(email)){
+            this.saveSubscriptions(email)
+        }else {
+            this.setState({
+                error:true
+            })
+        }
+    }
+
+    onChangeInput = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    render() {
+        return (
+            <div className="subcribe_panel">
+                <h3>Subscribe to us</h3>
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text"
+                            value={this.state.email}
+                            placeholder="yourmail@gmail.com"
+                            onChange={this.onChangeInput} />
+<div className={this.state.error ? "error show" : "error"}>Check your mail</div>
+<div className={this.state.success ? "success show" : "success"}>Thank you</div>
+                    </form>
+                </div>
+
+                <small>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua.
+                    </small>
+            </div>
+        )
+    }
+}
+
+export default SubScriptions;
